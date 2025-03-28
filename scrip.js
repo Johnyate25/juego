c08f00758af02252a4e712502c550594bfc6e36e
Johnyate25 <johnandersonyate04@gmail.com>
2025-03-28 04:08:57 +0000

    pruebaa1

---
+++ scrip.js
@@ -0,0 +1,13 @@
+        // Configuración de Firebase
+        const firebaseConfig = {
+            apiKey: "AIzaSyB_l-1oJlZl6vT818fxaNVVUVuO4G6jpYI",
+            authDomain: "juego-747e1.firebaseapp.com",
+            projectId: "juego-747e1",
+            storageBucket: "juego-747e1.firebasestorage.app",
+            messagingSenderId: "562456925140",
+            appId: "1:562456925140:web:cf02a0a252377b3b7b3c41"
+        };
+
+    firebase.initializeApp(firebaseConfig);
+    const db = firebase.firestore();
+
